e997f06ce2b74ecde4f85d04a1b31369141cff43
hussainbillah <wnnbdonline@gmail.com>
2025-04-12 07:04:14 +0000

    Pages uodate

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { showRegisterForm, registerUser } = require('../controllers/authController');
 
-router.get('/register', showRegisterForm);
-router.post('/register', registerUser);
+router.get('pages/register', showRegisterForm);
+router.post('pages/register', registerUser);
 
 module.exports = router;
